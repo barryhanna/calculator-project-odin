04ec9c98a96021d7402790a89759e04777d4ca7c
Barry Hanna <barryahanna@gmail.com>
2022-08-16 17:32:24 +0100

    Added features

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ let operator;
 
 const getDisplay = () => display.textContent;
 const setDisplay = (str) => (display.textContent = str);
-const digitHandler = (e) => (display.textContent += e.target.dataset.value);
+const digitHandler = (e) => {
+  if (display.textContent.includes('.') && e.target.dataset.value === '.') {
+    return;
+  }
+  display.textContent += e.target.dataset.value;
+};
 
 digitButtons.forEach((btn) => btn.addEventListener('click', digitHandler));
 
@@ -44,12 +49,17 @@ opButtons.forEach((btn) =>
 );
 
 equalButton.addEventListener('click', (e) => {
-  if (getDisplay() !== '') {
+  if (getDisplay() !== '' && operandOne) {
     operandOne = operate(operator, operandOne, getDisplay());
-    setDisplay(operandOne);
+    setDisplay(Math.round(operandOne, 2));
   }
 });
-clearButton.addEventListener('click', (e) => {});
+
+clearButton.addEventListener('click', (e) => {
+  operandOne = undefined;
+  operator = undefined;
+  setDisplay('');
+});
 
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
@@ -57,16 +67,16 @@ const divide = (num1, num2) => num1 / num2;
 const multiply = (num1, num2) => num1 * num2;
 
 const operate = (op, num1, num2) => {
-  console.log(`${num1} ${op} ${num2}`);
+  // console.log(`${num1} ${op} ${num2}`);
   switch (op) {
     case '+':
-      return add(Number.parseInt(num1), Number.parseInt(num2));
+      return add(Number.parseFloat(num1), Number.parseFloat(num2));
     case '-':
-      return subtract(Number.parseInt(num1), Number.parseInt(num2));
+      return subtract(Number.parseFloat(num1), Number.parseFloat(num2));
     case '/':
-      return divide(Number.parseInt(num1), Number.parseInt(num2));
+      return divide(Number.parseFloat(num1), Number.parseFloat(num2));
     case 'x':
-      return multiply(Number.parseInt(num1), Number.parseInt(num2));
+      return multiply(Number.parseFloat(num1), Number.parseFloat(num2));
     default:
       return;
   }
